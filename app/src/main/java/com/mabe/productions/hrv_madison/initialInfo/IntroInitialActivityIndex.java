741e7bdceed3b311f1295178d47c797a201cc2f2
martynasd123 <dargis.martynas@gmail.com>
2019-10-06 02:25:24 +0300

    removed obsolete library

--- app/src/main/java/com/mabe/productions/hrv_madison/initialInfo/IntroInitialActivityIndex.java
+++ app/src/main/java/com/mabe/productions/hrv_madison/initialInfo/IntroInitialActivityIndex.java
@@ -13,7 +13,6 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.github.channguyen.rsv.RangeSliderView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
